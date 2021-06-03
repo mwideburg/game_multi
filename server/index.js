cdba0f30dd355ceca33339c0ee79b28cbc1c611d
Michael Wideburg <mwideburg@gmail.com>
2021-06-03 14:39:52 -0400

    heroku push

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
-const http = require('http').createServer(app)
 const express = require('express');
 const app = express()
+const http = require('http').createServer(app)
 const io = require('socket.io')(http)
 const cors = require('cors')
 const path = require('path');
