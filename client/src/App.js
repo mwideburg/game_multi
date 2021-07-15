8e92596797f90dfafb22aa47a0cb8905b09904c8
Michael Wideburg <mwideburg@gmail.com>
2021-07-15 19:38:22 -0400

    error message for mobile or tablets

--- client/src/App.js
+++ client/src/App.js
@@ -10,29 +10,39 @@ import { UsersProvider } from './usersContext'
 
 import DefaultPage from './components/DefaultPage'
 import Game from './components/Game/game'
+var is_mobile = !!navigator.userAgent.match(/iphone|android|blackberry/ig) || false;
 function App() {
+  if(!is_mobile){
+    return (
+      <ChakraProvider>
+        <MainProvider>
+          <UsersProvider>
+            <SocketProvider>
+              <Flex className="App" align='center' flexDirection="column" justifyContent='center'>
+                <HashRouter>
+                  <Switch>
+                    <Route exact path='/' component={Login} />
+  
+                    <Route exact path='/game/:room' component={Game} />
+                    <Route exact path='/chat/:room' component={Chat} />
+                    <Route component={DefaultPage} />
+                  </Switch>
+                </HashRouter>
+              </Flex>
+            </SocketProvider>
+          </UsersProvider>
+        </MainProvider>
+      </ChakraProvider>
+    );
 
-  return (
-    <ChakraProvider>
-      <MainProvider>
-        <UsersProvider>
-          <SocketProvider>
-            <Flex className="App" align='center' flexDirection="column" justifyContent='center'>
-              <HashRouter>
-                <Switch>
-                  <Route exact path='/' component={Login} />
-
-                  <Route exact path='/game/:room' component={Game} />
-                  <Route exact path='/chat/:room' component={Chat} />
-                  <Route component={DefaultPage} />
-                </Switch>
-              </HashRouter>
-            </Flex>
-          </SocketProvider>
-        </UsersProvider>
-      </MainProvider>
-    </ChakraProvider>
-  );
+  }else{
+    return(
+      <Flex className="Mobile" align='center' flexDirection="column" justifyContent='center'>
+        Sorry, this app is not yet supported on mobile or tablets
+        <a href="https://mwideburg.github.io/"> Michael Wideburg's Portfolio</a>
+      </Flex>
+    )
+  }
 }
 
 export default App;
